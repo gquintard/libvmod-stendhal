757a99e568ae12eeb4f36cdb7a2ffcd2ae104d14
Guillaume Quintard <guillaume@varnish-software.com>
2016-08-26 21:33:52 +0200

    first draft

--- src/vmod_stendhal.c
+++ src/vmod_stendhal.c
@@ -1,87 +1,142 @@
-#include "config.h"
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
 
-/* need vcl.h before vrt.h for vmod_evet_f typedef */
-#include "vcl.h"
-#include "vrt.h"
-#include "cache/cache.h"
+#include <vcl.h>
+#include <vrt.h>
+#include <vtree.h>
+#include <cache/cache.h>
 
-#include "vtim.h"
 #include "vcc_if.h"
 
-const size_t infosz = 64;
-char	     *info;
+struct node {
+	unsigned		magic;
+#define MFD_NODE_MAGIC		0xc5daec29
+	char			*idx;
+	VCL_BACKEND		be;
+	VRB_ENTRY(node)		tr;
+};
 
-/*
- * handle vmod internal state, vmod init/fini and/or varnish callback
- * (un)registration here.
- *
- * malloc'ing the info buffer is only indended as a demonstration, for any
- * real-world vmod, a fixed-sized buffer should be a global variable
- */
+VRB_HEAD(backend_tree, node);
 
-int __match_proto__(vmod_event_f)
-event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
+struct vmod_stendhal_director {
+	unsigned		magic;
+#define MFD_MAGIC		0xa99e570a
+	pthread_rwlock_t	mtx;
+	struct backend_tree	bet;
+};
+
+inline int
+cmp_idx(const struct node *a, const struct node *b)
 {
-	char	   ts[VTIM_FORMAT_SIZE];
-	const char *event = NULL;
-
-	(void) ctx;
-	(void) priv;
-
-	switch (e) {
-	case VCL_EVENT_LOAD:
-		info = malloc(infosz);
-		if (! info)
-			return (-1);
-		event = "loaded";
-		break;
-	case VCL_EVENT_WARM:
-		event = "warmed";
-		break;
-	case VCL_EVENT_COLD:
-		event = "cooled";
-		break;
-	case VCL_EVENT_DISCARD:
-		free(info);
-		return (0);
-		break;
-	default:
-		return (0);
-	}
-	AN(event);
-	VTIM_format(VTIM_real(), ts);
-	snprintf(info, infosz, "vmod_stendhal %s at %s", event, ts);
-
-	return (0);
+	return (strcmp(a->idx, b->idx));
 }
 
-VCL_STRING
-vmod_info(VRT_CTX)
-{
-	(void) ctx;
+VRB_PROTOTYPE_STATIC(backend_tree, node, tr, cmp_idx);
+VRB_GENERATE_STATIC(backend_tree, node, tr, cmp_idx);
 
-	return (info);
+VCL_VOID __match_proto__()
+vmod_director__init(VRT_CTX,
+    struct vmod_stendhal_director **sd,
+    const char *vcl_name)
+{
+	struct vmod_stendhal_director *d;
+
+	(void)ctx;
+	(void)vcl_name;
+	ALLOC_OBJ(d, MFD_MAGIC);
+	AZ(pthread_rwlock_init(&d->mtx, NULL));
+	VRB_INIT(&d->bet);
+	*sd = d;
 }
 
-VCL_STRING
-vmod_hello(VRT_CTX, VCL_STRING name)
-{
-	char *p;
-	unsigned u, v;
 
-	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
-	p = ctx->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
-	v++;
-	if (v > u) {
-		/* No space, reset and leave */
-		WS_Release(ctx->ws, 0);
-		return (NULL);
+VCL_VOID __match_proto__()
+vmod_director__fini(struct vmod_stendhal_director **sd)
+{
+	struct node *nd, *nnd;
+	struct vmod_stendhal_director *d;
+
+	AN(sd);
+	d = *sd;
+	CHECK_OBJ_NOTNULL(d, MFD_MAGIC);
+
+	for (nd = VRB_MIN(backend_tree, &d->bet);
+			nd != NULL; nd = nnd) {
+		nnd = VRB_NEXT(backend_tree, &d->bet, nd);
+		VRB_REMOVE(backend_tree, &d->bet, nd);
+		free(nd->idx);
+		free(nd);
 	}
-	/* Update work space with what we've used */
-	WS_Release(ctx->ws, v);
-	return (p);
+	AZ(pthread_rwlock_destroy(&d->mtx));
+}
+
+VCL_VOID __match_proto__()
+vmod_director_add_backend(VRT_CTX, struct vmod_stendhal_director *sd,
+		VCL_STRING idx, const VCL_BACKEND be)
+{
+	struct node *nd, tmp = {0};
+	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+	CHECK_OBJ_NOTNULL(sd, MFD_MAGIC);
+
+	tmp.idx = strdup(idx ? idx : "");
+
+	pthread_rwlock_wrlock(&sd->mtx);
+	nd = VRB_FIND(backend_tree, &sd->bet, &tmp);
+	if (nd) {
+		nd->be = be;
+	} else {
+		ALLOC_OBJ(nd, MFD_NODE_MAGIC);
+		nd->idx = tmp.idx;
+		tmp.idx = NULL;
+		nd->be = be;
+		VRB_INSERT(backend_tree, &sd->bet, nd);
+	}
+	pthread_rwlock_unlock(&sd->mtx);
+
+	free(tmp.idx);
+}
+
+VCL_VOID __match_proto__()
+vmod_director_remove_backend(VRT_CTX, struct vmod_stendhal_director *sd,
+		VCL_STRING idx)
+{
+	struct node *nd, tmp = {0};
+	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+	CHECK_OBJ_NOTNULL(sd, MFD_MAGIC);
+
+	tmp.idx = strdup(idx ? idx : "");
+
+	pthread_rwlock_wrlock(&sd->mtx);
+	nd = VRB_FIND(backend_tree, &sd->bet, &tmp);
+	if (nd) { 
+		VRB_REMOVE(backend_tree, &sd->bet, nd);
+		free(nd->idx);
+		free(nd);
+	}
+	pthread_rwlock_unlock(&sd->mtx);
+
+	free(tmp.idx);
+}
+
+VCL_BACKEND __match_proto__()
+vmod_director_backend(VRT_CTX, struct vmod_stendhal_director *sd,
+		VCL_STRING idx)
+{
+	VCL_BACKEND be = NULL;
+	struct node *nd, tmp = {0};
+
+	tmp.idx = strdup(idx ? idx : "");
+
+	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+	CHECK_OBJ_NOTNULL(sd, MFD_MAGIC);
+	pthread_rwlock_rdlock(&sd->mtx);
+	nd = VRB_FIND(backend_tree, &sd->bet, &tmp);
+	if (nd)
+		be = nd->be;
+	pthread_rwlock_unlock(&sd->mtx);
+
+	free(tmp.idx);
+	return (be);
 }
